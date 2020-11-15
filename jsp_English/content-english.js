ab19074906656c1b82bcd5f3ffe0efc4eba5d077
Jhaycee Llaneta <43661903+johncarlosllaneta@users.noreply.github.com>
2020-11-15 22:22:43 +0800

    sql connection

--- jsp_English/content-english.js
+++ jsp_English/content-english.js
@@ -322,7 +322,7 @@ function powerpoint(url) {
 }
 
 function hi() {
-    window.location.href = "/English Page/assessment_english.html";
+    window.location.href = "../English Page/assessment_english.html";
 }
 
 function createAssessmentBtn() {
