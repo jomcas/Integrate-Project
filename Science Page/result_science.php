ad86303ec894df0a6910777747f0d6eba129a53e
ghiedoc <ghiedoc@gmail.com>
2020-11-15 20:17:18 +0800

    Update table
    removed 1/10 part

--- Science Page/result_science.php
+++ Science Page/result_science.php
@@ -3,23 +3,24 @@
 <?php session_start(); ?>
 
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Subject Results</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Subject Results</title>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="shortcut icon" href="../images/logo.png" type="image/png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="../images/logo.png" type="image/png">
 
-<link rel="stylesheet" href="../css/result_science.css">
+    <link rel="stylesheet" href="../css/result_science.css">
 
 
 </head>
+
 <body>
     <?php
     echo $_SESSION['User_Id'];
@@ -45,12 +46,14 @@
 
         <div class="table-title">
             <div class="row">
-                <div><p>Science Results</p></div>      
-             </div>
+                <div>
+                    <p>Science Results</p>
+                </div>
+            </div>
         </div>
         <table class="table table-striped table-hover">
-            <thead> 
-                <tr> 
+            <thead>
+                <tr>
                     <th>Lesson</th>
                     <th>Scores</th>
                     <th>Passing Score</th>
@@ -59,13 +62,13 @@
                 </tr>
             </thead>
             <tbody>
-        
-            <?php
+
+                <?php
             // Populate Table
             while($row = $result->fetch_assoc()) {
                     echo '<tr>';
                     echo '<td>' .$row['Lesson_Name']. '</td>';
-                    echo '<td>' .$row['Score']. '/' .$row['No_Items']. '</td>';
+                    echo '<td>' .$row['Score']. '</td>';
                     echo '<td>' .$row['Passing_Score']. '</td>';
                     echo '<td>' .$row['No_Items']. '</td>';
                     echo "<td><span class='label label-success' style='background-color: #332393;'>" .$row['Status']. '</td>';
@@ -76,8 +79,9 @@
 
             </tbody>
         </table>
-    </div> 
-<script src="javascript.js"></script>
-    
+    </div>
+    <script src="javascript.js"></script>
+
 </body>
-</html>                       
+
+</html>
